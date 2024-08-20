955da1e8769d13ca8b24c5419e26a1f436d56e34
Marcus Bragion <viniciusbragion@outlook.pt>
2024-08-20 09:24:30 -0300

    feat: dbt-core set up

--- src/extract_load.py
+++ src/extract_load.py
@@ -15,7 +15,8 @@ DB_USER_PRD = os.getenv("DB_USER_PRD")
 DB_PASS_PRD = os.getenv("DB_PASS_PRD")
 DB_HOST_PRD = os.getenv("DB_HOST_PRD")
 
-DATABASE_URL = f"postgresql://{DB_USER_PRD}:{DB_PASS_PRD}@{DB_HOST_PRD}:{DB_PORT_PRD}/{DB_NAME_PRD}"
+DATABASE_URL = f"postgresql://{DB_USER_PRD}:{DB_PASS_PRD}@{DB_HOST_PRD}:\
+{DB_PORT_PRD}/{DB_NAME_PRD}"
 
 # etl
 engine = create_engine(DATABASE_URL)
@@ -23,13 +24,35 @@ commodities = ["CL=F", "GC=F", "SI=F"]
 
 
 def buscar_dados_commodities(simbolo, periodo="5y", intervalo="1d"):
+    """
+    função que busca os dados de um ativo no Yahoo Finance e retorna um
+    DataFrame.
+
+    args:
+        simbolo (str): o ticker do ativo
+        periodo (str): o tempo de preços que deseja obter
+        intervalo (str): o tempo em que deseja obter os dados
+
+    returns:
+        DataFrame: os dados do ativo em um DataFrame
+    """
     ticker = yf.Ticker("CL=F")
-    dados = ticker.history(period=periodo, interval=intervalo)[["Close"]]
+    dados = ticker.history(period=periodo, interval=intervalo)
     dados["simbolo"] = simbolo
     return dados
 
 
 def buscar_todos_dados_commodities(simbolos):
+    """
+    função que busca os dados de todos os ativos no Yahoo Finance e retorna
+    um DataFrame.
+
+    args:
+        simbolos (list): uma lista com os tickers dos ativos
+
+    returns:
+        DataFrame: os dados dos ativos em um DataFrame
+    """
     todos_dados = []
     for simbolo in commodities:
         dados = buscar_dados_commodities(simbolo)
@@ -39,7 +62,18 @@ def buscar_todos_dados_commodities(simbolos):
 
 
 def salvar_no_postgres(df, schema="public"):
-    df.to_sql("commodities", engine, schema=schema, if_exists="replace", index=False)
+    """
+    função que salva os dados em um DataFrame no PostgreSQL.
+
+    args:
+        df (DataFrame): os dados que deseja salvar
+        schema (str): o schema que deseja salvar os dados
+
+    returns:
+        None
+    """
+    df.to_sql("commodities", engine, schema=schema, if_exists="replace",
+              index=False)
 
 
 if __name__ == "__main__":
