303024ddee6ff69189432a67a0e5a34f1cf43bb2
Marcus Bragion <viniciusbragion@outlook.pt>
2024-08-19 23:01:26 -0300

    feat: extract_load.py completed

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,23 +1,33 @@
 # imports
 import os
-import requests
 
 import pandas as pd
-import psycopg2
-import sqlalchemy
 import yfinance as yf
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
-# env
+# .env
+load_dotenv()
+
+DB_PORT_PRD = os.getenv("DB_PORT_PRD")
+DB_NAME_PRD = os.getenv("DB_NAME_PRD")
+DB_USER_PRD = os.getenv("DB_USER_PRD")
+DB_PASS_PRD = os.getenv("DB_PASS_PRD")
+DB_HOST_PRD = os.getenv("DB_HOST_PRD")
+
+DATABASE_URL = f"postgresql://{DB_USER_PRD}:{DB_PASS_PRD}@{DB_HOST_PRD}:{DB_PORT_PRD}/{DB_NAME_PRD}"
 
 # etl
-
+engine = create_engine(DATABASE_URL)
 commodities = ["CL=F", "GC=F", "SI=F"]
 
+
 def buscar_dados_commodities(simbolo, periodo="5y", intervalo="1d"):
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker("CL=F")
     dados = ticker.history(period=periodo, interval=intervalo)[["Close"]]
-    dados['simbolo'] = simbolo
+    dados["simbolo"] = simbolo
     return dados
+
 
 def buscar_todos_dados_commodities(simbolos):
     todos_dados = []
@@ -28,6 +38,10 @@ def buscar_todos_dados_commodities(simbolos):
     return pd.concat(todos_dados)
 
 
+def salvar_no_postgres(df, schema="public"):
+    df.to_sql("commodities", engine, schema=schema, if_exists="replace", index=False)
+
+
 if __name__ == "__main__":
     dados_concatenado = buscar_todos_dados_commodities(commodities)
-    print(dados_concatenado)
+    salvar_no_postgres(dados_concatenado)
